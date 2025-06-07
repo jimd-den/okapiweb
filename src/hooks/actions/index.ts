f26d7eed3f3d8de550b2ef34148525f9e2d02379
G Monk <violindocgfx@gmail.com>
2025-06-07 17:57:36 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/hooks/actions/index.ts
+++ src/hooks/actions/index.ts
@@ -1,3 +1,3 @@
 // src/hooks/actions/index.ts
-export * from './useSpaceActionLogger';
+export * from './use-space-action-logger';
 // use-action-logger.ts is a placeholder, so not exported
--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,10 +1,10 @@
 // src/hooks/index.ts
 export * from './use-dialog-state';
 export * from './use-editable-item';
-export * from './useFormWizardLogic';
-export * from './useImageCaptureDialog';
-export * from './useMobile';
-export * from './useSpaceDialogs';
+export * from './use-form-wizard-logic';
+export * from './use-image-capture-dialog';
+export * from './use-mobile';
+export * from './use-space-dialogs';
 export * from './use-action-definition-form'; // formerly use-create-action-definition-form
 
 // Re-export from sub-barrels
